ef0ba3964f5c6c24ed0ab517389e7980749f5bde
Preslav977 <p.cvetanow@gmail.com>
2025-07-07 09:02:37 +0300

    Decided to let go of the scrolling text animation, since it wasnt able to make it repeat like a infinite loop for each grid. Added figure and figcaption for each image

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -3,75 +3,169 @@ import styles from "./Skills.module.css";
 export function Skills() {
   return (
     <div className={styles.flexedCenteredSkillsContainer}>
+      <h3>Skills</h3>
       <section className={styles.gridSkillsContainer}>
         <div className={styles.flexedFrontEndSkillsContainer}>
           <p>Front-end</p>
           <div className={styles.flexedFrontEndSvgContainer}>
-            <img className={styles.skillsSvg} src="./html5.svg" alt="html5" />
-            <img className={styles.skillsSvg} src="./css3.svg" alt="css3" />
-            <img
-              className={styles.skillsSvg}
-              src="./javascript.svg"
-              alt="javascript"
-            />
-            <img className={styles.skillsSvg} src="./react.svg" alt="react" />
+            <figure>
+              <img className={styles.skillsSvg} src="./html5.svg" alt="html5" />
+              <figcaption>HTML5</figcaption>
+            </figure>
+            <figure>
+              <img className={styles.skillsSvg} src="./css3.svg" alt="css3" />
+              <figcaption>CSS3</figcaption>
+            </figure>
+            <figure>
+              <img
+                className={styles.skillsSvg}
+                src="./javascript.svg"
+                alt="javascript"
+              />
+              <figcaption>JavaScript</figcaption>
+            </figure>
+            <figure>
+              <img className={styles.skillsSvg} src="./react.svg" alt="react" />
+              <figcaption>React</figcaption>
+            </figure>
           </div>
         </div>
         <div className={styles.flexedBackEndSkillsContainer}>
           <p>Back-end</p>
           <div className={styles.flexedBackEndSvgContainer}>
-            <img className={styles.skillsSvg} src="./nodejs.svg" alt="nodejs" />
-            <img
-              className={styles.skillsSvg}
-              src="./express.svg"
-              alt="express"
-            />
-            <img
-              className={styles.skillsSvg}
-              src="./javascript.svg"
-              alt="javascript"
-            />
-            <img
-              className={styles.skillsSvg}
-              src="./mongodb.svg"
-              alt="mongodb"
-            />
-            <img
-              className={styles.skillsSvg}
-              src="./postgresql.svg"
-              alt="postgresql"
-            />
-            <img className={styles.skillsSvg} src="./prisma.svg" alt="prisma" />
-            <img className={styles.skillsSvg} src="./pug.svg" alt="pug" />
-            <img className={styles.skillsSvg} src="./jest.svg" alt="jest" />
+            <figure>
+              <img
+                className={styles.skillsSvg}
+                src="./nodejs.svg"
+                alt="nodejs"
+              />
+              <figcaption>NodeJS</figcaption>
+            </figure>
+            <figure>
+              <img
+                className={styles.skillsSvg}
+                src="./express.svg"
+                alt="express"
+              />
+              <figcaption>Express</figcaption>
+            </figure>
+            <figure>
+              <img
+                className={styles.skillsSvg}
+                src="./javascript.svg"
+                alt="javascript"
+              />
+              <figcaption>JavaScript</figcaption>
+            </figure>
+            <figure>
+              <img
+                className={styles.skillsSvg}
+                src="./mongodb.svg"
+                alt="mongodb"
+              />
+              <figcaption>MongoDB</figcaption>
+            </figure>
+            <figure>
+              <img
+                className={styles.skillsSvg}
+                src="./postgresql.svg"
+                alt="postgresql"
+              />
+              <figcaption>PostgreSQL</figcaption>
+            </figure>
+            <figure>
+              <img
+                className={styles.skillsSvg}
+                src="./prisma.svg"
+                alt="prisma"
+              />
+              <figcaption>Prisma</figcaption>
+            </figure>
+            <figure>
+              <img className={styles.skillsSvg} src="./pug.svg" alt="pug" />
+              <figcaption>Pug / Jade</figcaption>
+            </figure>
+            <figure>
+              <img className={styles.skillsSvg} src="./jest.svg" alt="jest" />
+              <figcaption>Jest</figcaption>
+            </figure>
           </div>
         </div>
         <div className={styles.flexedMiscellaneousSkillsContainer}>
           <p>Miscellaneous</p>
           <div className={styles.flexedMiscellaneousSvgContainer}>
-            <img className={styles.skillsSvg} src="./git.svg" alt="git" />
-            <img className={styles.skillsSvg} src="./github.svg" alt="github" />
-            <img
-              className={styles.skillsSvg}
-              src="./webpack.svg"
-              alt="webpack"
-            />
-            <img className={styles.skillsSvg} src="./vite.svg" alt="vite" />
-            <img className={styles.skillsSvg} src="./npm.svg" alt="npm" />
-            <img
-              className={styles.skillsSvg}
-              src="./postman.svg"
-              alt="postman"
-            />
-            <img className={styles.skillsSvg} src="./vscode.svg" alt="vscode" />
-            <img className={styles.skillsSvg} src="./eslint.svg" alt="eslint" />
-            <img
-              className={styles.skillsSvg}
-              src="./prettier.svg"
-              alt="prettier"
-            />
-            <img className={styles.skillsSvg} src="./vercel.svg" alt="vercel" />
-            <img className={styles.skillsSvg} src="./linux.svg" alt="linux" />
+            <figure>
+              <img className={styles.skillsSvg} src="./git.svg" alt="git" />
+              <figcaption>Git</figcaption>
+            </figure>
+            <figure>
+              <img
+                className={styles.skillsSvg}
+                src="./github.svg"
+                alt="github"
+              />
+              <figcaption>Github</figcaption>
+            </figure>
+            <figure>
+              <img
+                className={styles.skillsSvg}
+                src="./webpack.svg"
+                alt="webpack"
+              />
+              <figcaption>Webpack</figcaption>
+            </figure>
+            <figure>
+              <img className={styles.skillsSvg} src="./vite.svg" alt="vite" />
+              <figcaption>Vite</figcaption>
+            </figure>
+            <figure>
+              <img className={styles.skillsSvg} src="./npm.svg" alt="npm" />
+              <figcaption>Npm</figcaption>
+            </figure>
+            <figure>
+              <img
+                className={styles.skillsSvg}
+                src="./postman.svg"
+                alt="postman"
+              />
+              <figcaption>Postman</figcaption>
+            </figure>
+            <figure>
+              <img
+                className={styles.skillsSvg}
+                src="./vscode.svg"
+                alt="vscode"
+              />
+              <figcaption>VSCode</figcaption>
+            </figure>
+            <figure>
+              <img
+                className={styles.skillsSvg}
+                src="./eslint.svg"
+                alt="eslint"
+              />
+              <figcaption>ESLint</figcaption>
+            </figure>
+            <figure>
+              <img
+                className={styles.skillsSvg}
+                src="./prettier.svg"
+                alt="prettier"
+              />
+              <figcaption>Prettier</figcaption>
+            </figure>
+            <figure>
+              <img
+                className={styles.skillsSvg}
+                src="./vercel.svg"
+                alt="vercel"
+              />
+              <figcaption>Vercel</figcaption>
+            </figure>
+            <figure>
+              <img className={styles.skillsSvg} src="./linux.svg" alt="linux" />
+              <figcaption>Linux</figcaption>
+            </figure>
           </div>
         </div>
       </section>
