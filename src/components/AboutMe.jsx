03eee6768140ae235e43439a6e5294fc89b46b56
Preslav977 <p.cvetanow@gmail.com>
2025-07-09 10:47:11 +0300

    Fixed overflowing issue for AboutMe, and added some styling

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -4,15 +4,15 @@ export function AboutMe() {
   return (
     <section className={styles.aboutMeSection} id="#about">
       <div className={styles.aboutMeInfoContainer}>
-        <h3>About me</h3>
-        <p>
+        <h3 className={styles.aboutMeHeader}>About me</h3>
+        <p className={styles.aboutMeInfoPara}>
           Hello, my name is Preslav. I am a full-stack developer from Bulgaria.
           My journey began in 2022 because I decided to try programming. I
           stumbled on The Odin Project, since then I learned so much about
           concepts, building projects, communicating with other people really
           neat free curriculum with awesome people.
         </p>
-        <p>
+        <p className={styles.aboutMeInfoPara}>
           Outside of programming I enjoy listening to music on the Monstercat
           label. watching documentations and working out outside.
         </p>
