732edc22f08781e5752949f8699582ef0537623d
Preslav977 <p.cvetanow@gmail.com>
2025-07-09 12:46:41 +0300

    Finished styling the ContactMe section

--- src/components/ContactMe.jsx
+++ src/components/ContactMe.jsx
@@ -2,38 +2,49 @@ import styles from "./ContactMe.module.css";
 
 export function ContactMe() {
   return (
-    <section className={styles.contactMeContainer} id="#contact">
-      <div>
-        <h6>Contact</h6>
+    <div className={styles.contactMeWrapper}>
+      <h6 className={styles.contactMeHeader}>Contact</h6>
+      <section className={styles.contactMeContainer} id="#contact">
+        <div>
+          <div className={styles.contactMeDotsContainer}>
+            <div className={styles.contactMeDots}></div>
+            <div className={styles.contactMeDots}></div>
+            <div className={styles.contactMeDots}></div>
+          </div>
+          <p className={styles.contactMePara}>
+            Feel free to contact me through any of the methods listed below.
+          </p>
 
-        <p>Feel free to contact me through any of the methods listed below.</p>
+          <div className={styles.contactMeSvgContainer}>
+            <a href="https://github.com/Preslav977" target="_blank">
+              <img
+                className={styles.contactMeSvg}
+                src="./github.svg"
+                alt="github"
+              />
+            </a>
 
-        <div className={styles.contactMeSvgContainer}>
-          <a href="https://github.com/Preslav977" target="_blank">
-            <img
-              className={styles.contactMeSvg}
-              src="./github.svg"
-              alt="github"
-            />
-          </a>
+            <a href="mailto:preslav.tsvetanov@gmail.com" target="_blank">
+              <img
+                className={styles.contactMeSvg}
+                src="./email.svg"
+                alt="email"
+              />
+            </a>
 
-          <a href="mailto:preslav.tsvetanov@gmail.com">
-            <img
-              className={styles.contactMeSvg}
-              src="./email.svg"
-              alt="email"
-            />
-          </a>
-
-          <a href="https://www.linkedin.com/in/preslav-tsvetanov/">
-            <img
-              className={styles.contactMeSvg}
-              src="./linkedin.svg"
-              alt="linkedin"
-            />
-          </a>
+            <a
+              href="https://www.linkedin.com/in/preslav-tsvetanov/"
+              target="_blank"
+            >
+              <img
+                className={styles.contactMeSvg}
+                src="./linkedin.svg"
+                alt="linkedin"
+              />
+            </a>
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </div>
   );
 }
