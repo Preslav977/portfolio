cc832354dc877240977a4e55d583e75d58e9f7b9
Preslav977 <p.cvetanow@gmail.com>
2025-09-03 13:07:54 +0300

    Added aditional containers to make the image on the same level as the paragragh text, and to avoid the image looking like an egg

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -3,21 +3,29 @@ import styles from "./AboutMe.module.css";
 export function AboutMe() {
   return (
     <section className={styles.aboutMeSection} id="about">
+      <h3 className={styles.aboutMeHeader}>About me</h3>
       <div className={styles.aboutMeInfoContainer}>
-        <h3 className={styles.aboutMeHeader}>About me</h3>
-        <p className={styles.aboutMeInfoPara}>
-          Hello, my name is Preslav. I am a full-stack developer from Bulgaria.
-          My journey began in 2022 because I decided to try programming. I
-          stumbled on The Odin Project, since then I learned so much about
-          concepts, building projects, communicating with other people really
-          neat free curriculum with awesome people.
-        </p>
-        <p className={styles.aboutMeInfoPara}>
-          Outside of programming I enjoy listening to music on the Monstercat
-          label. Watching documentations and working out outside.
-        </p>
+        <div className={styles.aboutMeInfo}>
+          <p className={styles.aboutMeInfoPara}>
+            Hello, my name is Preslav. I am a full-stack developer from
+            Bulgaria. My journey began in 2022 because I decided to try
+            programming. I stumbled on The Odin Project, since then I learned so
+            much about concepts, building projects, communicating with other
+            people really neat free curriculum with awesome people.
+          </p>
+          <p className={styles.aboutMeInfoPara}>
+            Outside of programming I enjoy listening to music on the Monstercat
+            label. Watching documentations and working out outside.
+          </p>
+        </div>
+        <div>
+          <img
+            className={styles.aboutMeImg}
+            src="./preslav.png"
+            alt="preslav"
+          />
+        </div>
       </div>
-      <img className={styles.aboutMeImg} src="./preslav.png" alt="preslav" />
     </section>
   );
 }
